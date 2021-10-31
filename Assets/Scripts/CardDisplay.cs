d2026b111cb2bdc46c11284f7049afe137544e10
sowrd299 <ryan.ward46@gmail.com>
2021-10-31 11:06:21 -0700

    A cach-based version of card display

--- Assets/Scripts/CardDisplay.cs
+++ Assets/Scripts/CardDisplay.cs
@@ -6,27 +6,38 @@ using UnityEngine;
 public class CardDisplay : MonoBehaviour
 {
 
+	private static Dictionary<string, Sprite> cachedSprites = new Dictionary<string, Sprite>();
+
+	
 	[SerializeField]
 	private string fileNameFormat = "Sprites/Cards/img_card_{0}{1:D2}"; // I don't like magic numbers... but thats how format works :(
 
-	[SerializeField]
 	private string clubsName = "c";
-	[SerializeField]
 	private string diamondsName = "d";
-	[SerializeField]
 	private string heartsName = "h";
-	[SerializeField]
 	private string spadesName = "s";
 
 
-	private Dictionary<Suit, string> suitNames;
+	private static Dictionary<Suit, string> _suitNames;
+	protected virtual Dictionary<Suit, string> suitNames {
+		get{
+			return _suitNames;
+		}
+		private set{
+			_suitNames = value;
+		}
+	}
 
+
+	// Called to initialize the display; must be called before the object can be used
 	public void Init(){
-		suitNames = new Dictionary<Suit, string>();
-		suitNames.Add(Suit.CLUBS, clubsName);
-		suitNames.Add(Suit.DIAMONDS, diamondsName);
-		suitNames.Add(Suit.HEARTS, heartsName);
-		suitNames.Add(Suit.SPADES, spadesName);
+		if(suitNames == null){
+			suitNames = new Dictionary<Suit, string>();
+			suitNames.Add(Suit.CLUBS, clubsName);
+			suitNames.Add(Suit.DIAMONDS, diamondsName);
+			suitNames.Add(Suit.HEARTS, heartsName);
+			suitNames.Add(Suit.SPADES, spadesName);
+		}
 	}
 
 
@@ -43,9 +54,18 @@ public class CardDisplay : MonoBehaviour
 		}
 	}
 
+
+	// Loads and displays the appropriate sprite (based on fileNameFormat)
+	// 		...for the given card
 	private Sprite loadSprite(Card card){
 		string fileName = String.Format(fileNameFormat, suitNames[card.Suit], card.Rank);
-		Sprite sprite = Resources.Load<Sprite>(fileName) as Sprite;
+		Sprite sprite;
+		if(cachedSprites.ContainsKey(fileName)){
+			sprite = cachedSprites[fileName];
+		}else{
+			sprite = Resources.Load<Sprite>(fileName) as Sprite;
+			cachedSprites[fileName] = sprite;
+		}
 		return sprite;
 	}
 
